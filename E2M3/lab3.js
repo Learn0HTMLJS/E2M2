852d7f395e2e81708a11876029e0a0787b39f48f
Roman S <svroev@gmail.com>
2022-03-14 19:23:10 +0300

    and...

--- E2M3/lab3.js
+++ E2M3/lab3.js
@@ -4,13 +4,18 @@ const Button = document.getElementById('Buttonka');
 const CloseButton = document.getElementById('CloseButton');
 const Row = document.getElementById('flBtn');
 const Submit = document.getElementById('BtnSb');
+const Name = document.getElementById('InputName');
+const EMAIL = document.getElementById('InputEMAIL');
+Dialog.addEventListener('click', NoUp);
 Button.addEventListener('click', ShowModal);
 CloseButton.addEventListener('click', CloseModal);
 Background.addEventListener('click', CloseModal);
 Submit.addEventListener('click', BlockSubmit);
+Name.addEventListener('blur', NameValidate);
+EMAIL.addEventListener('blur', EmailValidation);
 
-Dialog.style.opacity = '0';
-Dialog.style.visibility = 'hidden';
+//Dialog.style.opacity = '0';
+//Dialog.style.visibility = 'hidden';
 Dialog.style.background = 'white';
 Dialog.style.width = '60%';
 Dialog.style.position = 'fixed';
@@ -19,9 +24,11 @@ Dialog.style.left = '20%';
 
 Background.style.opacity = '0';
 Background.style.visibility = 'hidden';
+
 Background.style.background = 'rgba(0, 0, 0, 0.3)';
 Background.style.width = '100%';
 Background.style.height = '100%';
+
 Background.style.position = 'fixed';
 Background.style.top = '0px';
 Background.style.left = '0px';
@@ -29,30 +36,82 @@ Background.style.left = '0px';
 Row.style.display = 'flex';
 Row.style.flexDirection = 'row-reverse';
 
-
 function ShowModal()
 {
-    Dialog.style.opacity = '1';
-    Dialog.style.visibility = 'visible';
+//    Dialog.style.opacity = '1';
+//    Dialog.style.visibility = 'visible';
 
     Background.style.opacity = '1';
     Background.style.visibility = 'visible';
+/*    Background.style.background = 'rgba(0, 0, 0, 0.3)';
+    Background.style.width = '100%';
+    Background.style.height = '100%';*/
 
     document.getElementsByTagName('body')[0].style.overflow = 'hidden';
 }
 
 function CloseModal()
 {
-    Dialog.style.opacity = '0';
-    Dialog.style.visibility = 'hidden';
+//    Dialog.style.opacity = '0';
+//    Dialog.style.visibility = 'hidden';
     Background.style.opacity = '0';
     Background.style.visibility = 'hidden';
     document.getElementsByTagName('body')[0].style.overflow = 'visible';
+
+    //Event.stopPropagation();
+}
+
+function NoUp(Event)
+{
+    Event.stopPropagation();
 }
 
 function BlockSubmit(Event)
 {
     Event.preventDefault();
     console.table(document.getElementById('SUBMITION'));
-    Event.stopPropagation;
+    Event.stopPropagation();
+}
+
+function NameValidate(Event)
+{
+//    Event.preventDefault();
+    let st = Name.value;
+
+    Name.setCustomValidity('detbtbf');
+    //console.log(st);    
+    if(st == '')
+        alert('Error');
+    Name.focus();
+//    Event.preventDefault();
+//    Event.stopPropagation();
+}
+
+function EmailValidation(Event)
+{
+    Event.preventDefault();
+    let st = EMAIL.value;
+    let flag = 0;
+    let i = 0;
+    for(i; i < length(st); i++)
+    {
+        if(st[i] == '@')
+        {
+            flag++;
+            break;
+        }
+    }    
+    for(i; i < length(st); i++)
+    {
+        if(st[i] == '.')
+        {
+            flag++;
+            break;
+        }
+    }
+    if(flag != 2)
+    {
+        alert('Error');
+    }
+    Event.stopPropagation();
 }
